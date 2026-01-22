8eb73eec2eb4775da140d54ae20dee925022e106
Adrian Salazar Collazos <adrian.1009@live.com>
2026-01-22 16:07:44 -0400

    Main with all operations

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from Modules.suma import SumaDosNumeros
 from Modules.multiplicacion import MultiplicacionDeDosNumeros
+from Modules.resta import RestaDosNumeros
 
 sumador = SumaDosNumeros(15, 25)
 sumador.mostrar_resultado()
+
+restador = RestaDosNumeros(10, 4)
+restador.mostrar_resultado()
 
 multiplicador = MultiplicacionDeDosNumeros(4, 7)
 multiplicador.mostrar_resultado()
