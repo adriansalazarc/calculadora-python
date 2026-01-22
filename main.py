b9901215235ec2b8f0988f26637b06334d9d8b78
Adrian Salazar Collazos <adrian.1009@live.com>
2026-01-22 12:19:14 -0400

    clase resta funcionando bien

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from Modules.suma import SumaDosNumeros
+from Modules.resta import RestaDosNumeros
 
 sumador = SumaDosNumeros(15, 25)
 sumador.mostrar_resultado()
+
+restador = RestaDosNumeros(30, 16)
+restador.mostrar_resultado()
