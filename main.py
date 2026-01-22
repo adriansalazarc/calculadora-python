1d088b45ebb0e3898d7a516bd93fa7a02249c094
Adrian Salazar Collazos <adrian.1009@live.com>
2026-01-22 13:42:22 -0400

    Added 3 numbers product

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from Modules.multiplicacion import MultiplicacionDeDosNumeros
 sumador = SumaDosNumeros(15, 25)
 sumador.mostrar_resultado()
 
-multiplicador = MultiplicacionDeDosNumeros(4, 7)
+multiplicador = MultiplicacionDeDosNumeros(4, 2, 3)
 multiplicador.mostrar_resultado()
