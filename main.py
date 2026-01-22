dfe08f8602f843751b4445491c3d01079b4b78ad
Adrian Salazar Collazos <adrian.1009@live.com>
2026-01-22 12:26:38 -0400

    Clase multiplicacion funcionando OK

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from Modules.suma import SumaDosNumeros
+from Modules.multiplicacion import MultiplicacionDeDosNumeros
 
 sumador = SumaDosNumeros(15, 25)
 sumador.mostrar_resultado()
+
+multiplicador = MultiplicacionDeDosNumeros(4, 7)
+multiplicador.mostrar_resultado()
